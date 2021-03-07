7d90c2197648aa9c32de08d94c40d0eaf73b5efe
IgnatIvanov <ignativanov1996@mail.ru>
2021-03-07 15:15:19 +0300

    2021.03.07.1515
    Stage 2 complete.

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,5 +1,8 @@
-# write your code here
-
-
 print('Hello! My name is Aid.')
 print('I was created in 2021.')
+print('Please, remind me your name.')
+
+# reading a name
+name = str(input())
+
+print('What a great name you have, {}!'.format(name))
